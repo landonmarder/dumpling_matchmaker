a23fc24e47b174d4cdf413a0d812e77a1b35ef34
landonmarder <landon.marder@gmail.com>
2014-01-11 16:33:15 -0500

    user signs in

--- spec/features/user_submits_soup_request_spec.rb
+++ spec/features/user_submits_soup_request_spec.rb
@@ -18,13 +18,13 @@ feature 'user submits soup request',%q{
   scenario 'with valid attributes' do
     user = FactoryGirl.create(:user)
     visit root_path
-    click_link 'Sign In'
-    fill_in 'Email', with:(user.email)
-    fill_in 'Password', with:(user.password)
+    within(".user_email") { fill_in 'Email', with: user.email }
+    fill_in 'Password', with: user.password
     click_button 'Sign In'
+
     click_link 'Request Soup'
     choose('Dumpling Soup')
-    click_button 'Submit Request' 
+    click_button 'Submit Request'
     expect(page).to have_content('Request Submitted')
   end
 end
