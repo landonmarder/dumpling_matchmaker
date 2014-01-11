11705d2880c2dfed07f7f576f5979dfd53997b63
landonmarder <landon.marder@gmail.com>
2014-01-11 16:47:23 -0500

    add user_id to soup

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,12 +1,14 @@
 class RequestsController < ApplicationController
   def index
+    @requests = Request.all
   end
 
   def create
     @request = Request.new(request_params)
+    @request.user_id = current_user.id
 
     if @request.save
-      redirect_to request_path(@request),
+      redirect_to requests_path,
         notice: "Request Submitted"
     else
       render :new
@@ -19,9 +21,9 @@ class RequestsController < ApplicationController
 
   private
 
-  def request_params 
-    params.require(:request).permit(:soup)
+  def request_params
+    params.require(:request).permit(:soup, :current_user)
   end
 
-  
+
 end
