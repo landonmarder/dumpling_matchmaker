c443cffe58f362b910c29eaf4e783d37dbb79f0f
skellz <sherwin.kelly@gmail.com>
2014-01-14 10:39:52 -0500

    updated spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 require 'coveralls'
-coveralls.wear!
+Coveralls.wear!
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
